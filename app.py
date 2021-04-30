ea1c383a24e092fd3a98cea03f372f708e8fd968
Aattawut Nutlamyong <Aattawut.field@gmail.com>
2021-04-30 14:48:57 +0700

    งง

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ import pandas as pd
 
 
 app = Flask(__name__)
-#api = Api(app)
-cors = CORS(app)
+CORS(app)
+api = Api(app)
 
 
 @app.route('/')
@@ -32,6 +32,6 @@ class get_sentiment(Resource):
         return res
 
 
-cors.add_resource(get_sentiment, '/get_sentiment', endpoint='get_sentiment')
+api.add_resource(get_sentiment, '/get_sentiment', endpoint='get_sentiment')
 if __name__ == '__main__':
     app.run(threaded=True)
